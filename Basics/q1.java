347047ad078078e621619383e400b2388d1db660
Suprita.T <suprita.t1805@gmail.com>
2025-02-16 15:38:49 +0000

    Modified q1.java

--- Basics/q1.java
+++ Basics/q1.java
@@ -1,1 +1,19 @@
-Created q1.java
+// Area of Circle
+package Basics;
+
+import java.util.Scanner;
+
+class q1 {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle : ");
+        double radius = num.nextDouble();
+
+        double area = Math.PI * radius * radius;
+
+        System.out.println("The area of the circle is : " + area);
+        
+    }
+}
---
+++ q7.java
@@ -0,0 +1,7 @@
+// To calculate Fibonacci series to n numbers.
+public class q7 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
