2787566c7d1a48a56e687024009279400cdb233d
Suprita.T <suprita.t1805@gmail.com>
2025-02-16 07:53:33 +0000

    Modified q2.java

---
+++ q2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class q2 {
+    public static void main(String[] args) {
+        Scanner words = new Scanner(System.in);
+        String name = words.next();
+        System.out.println("Welcome to VS Code ," + name);
+    }
+}
