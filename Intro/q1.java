98a088dc00b3ad977515031ef5fd63eb94065ad1
Suprita.T <suprita.t1805@gmail.com>
2025-03-06 07:12:27 +0000

    Added q1.java

--- Intro/q1.java
+++ Intro/q1.java
@@ -1,1 +1,20 @@
+package Intro;
 
+import java.util.Scanner;
+
+public class q1 {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        int N = num.nextInt();
+
+        if(N%2 == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
